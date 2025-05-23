ae1626faaafd5698c0fd1d921385d36320662e08
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-05-23 14:26:04 +0530

    deps: prune cmake, clang

--- flake.nix
+++ flake.nix
@@ -3,17 +3,24 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
         architecture = builtins.elemAt (pkgs.lib.splitString "-" system) 0;
-         libvosk = pkgs.stdenv.mkDerivation {
+        version = "0.3.45";
+        libvosk = pkgs.stdenv.mkDerivation {
           name = "libvosk";
           pname = "libvosk";
 
           src = pkgs.fetchurl {
-            url = "https://github.com/alphacep/vosk-api/releases/download/v0.3.45/vosk-linux-${architecture}-0.3.45.zip";
+            url = "https://github.com/alphacep/vosk-api/releases/download/v${version}/vosk-linux-${architecture}-${version}.zip";
             sha256 = "sha256-u9yO2FxDl59kQxQoiXcOqVy/vFbP+1xdzXOvqHXF+7I=";
           };
 
@@ -22,29 +29,26 @@
 
           installPhase = ''
             mkdir -p $out/lib
-            mv vosk-linux-x86_64-0.3.45/* $out/lib
+            mv vosk-linux-x86_64-${version}/* $out/lib
           '';
         };
-         in
-        {
-          devShells.default = pkgs.mkShell rec {
+      in
+      {
+        devShells.default = pkgs.mkShell rec {
 
-            packages = with pkgs;
-            [
-              libvosk
-              rustc
-              cargo
-              cmake
-              clang
-              pkg-config
-              alsa-lib
-              stdenv.cc.cc.lib
-              openssl.dev
-            ];
+          packages = with pkgs; [
+            libvosk
+            rustc
+            cargo
+            pkg-config
+            alsa-lib
+            stdenv.cc.cc.lib
+            openssl.dev
+          ];
 
-            LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath packages}";
-            LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib";
-          };
-        }
-      );
+          LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath packages}";
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+        };
+      }
+    );
 }
